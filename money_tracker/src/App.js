23449242066e25e4a14f15beb19721fa0d2f3b67
XiaoqianDing <dxq3442@gmail.com>
2020-02-19 10:32:08 +1100

    PriceList Component

--- money_tracker/src/App.js
+++ money_tracker/src/App.js
@@ -1,33 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import PriceList from './components/PriceList'
 
 const items = [
   {
     'id':1,
-    'title':'travel',
+    'title':'travel in Astralia',
     'price':200,
-    'date':'2018-09-10'
+    'date':'2018-09-10',
+    'category':{
+      'id':'1',
+      'name':'travel',
+      'type':'outcome'
+    }
+  },
+  {
+    'id':2,
+    'title':'travel in England',
+    'price':400,
+    'date':'2018-09-10',
+    'category':{
+      'id':'1',
+      'name':'travel',
+      'type':'outcome'
+    }
   }
 ]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <PriceList 
+            items = {items}
+            onModifyItem = {(item)=>{alert(item.id)}}
+            onDeleteItem = {(item)=>{alert(item.id)}}
+        ></PriceList>
     </div>
   );
 }
